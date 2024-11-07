37faad9a3bc336cb11fcb67383d75d13443ee425
tora180405 <toradigda@gmail.com>
2024-11-07 10:17:40 +0700

    fix middleware logic

--- php/app/controllers/DashboardAdmin.php
+++ php/app/controllers/DashboardAdmin.php
@@ -2,12 +2,7 @@
 
 class DashboardAdmin extends Controller{
     public function index(){
-        session_start();
-        if (!isset($_SESSION['user_id'])) {
-            header('Location: ' . BASEURL . '/login');
-            exit;
-        }else{
-            $this->view('admin/admin_dashboard');
-        }
+        $this->checkLogin();
+        $this->view('admin/admin_dashboard');
     }
 }
