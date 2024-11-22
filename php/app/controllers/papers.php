349ad0b04ed05835756ab2bc1eeb7de9f82d0b99
tora180405 <toradigda@gmail.com>
2024-11-22 07:49:22 +0700

    add preview pdf function

--- php/app/controllers/papers.php
+++ php/app/controllers/papers.php
@@ -1,5 +1,7 @@
 <?php
 
+use Dompdf\Dompdf;
+use Dompdf\Options;
 class Papers extends Controller{
     public function index(){
         
@@ -27,5 +29,55 @@ class Papers extends Controller{
         }else{
             header('Location: ' . $this->getLastVisitedPage());
         }
+    }
+
+    public function createPaper(){
+        
+        $researchTitle = $_POST["researchTitle"];
+        $leadResearcher = $_POST["leadResearcher"];
+        $researchScheme = $_POST["researchScheme"];
+        $researchCenter = $_POST["researchCenter"];
+        $researchTopic = $_POST["researchTopic"];
+        $date = $this->tgl_indo(date('Y-m-d'));
+
+        $options = new Options();
+        $options->setIsRemoteEnabled(true);
+        $options->setChroot(__DIR__);
+        $dompdf = new Dompdf($options);
+
+        $html = file_get_contents( ASSETS . "/components/Doc/styleSurat.html");
+        $html = str_replace("{{ researchTitle }}", $researchTitle, $html);
+        $html = str_replace("{{ leadResearcher }}", $leadResearcher, $html);
+        $html = str_replace("{{ researchScheme }}", $researchScheme, $html);
+        $html = str_replace("{{ researchCenter }}", $researchCenter, $html);
+        $html = str_replace("{{ researchTopic }}", $researchTopic, $html);
+        $html = str_replace("{{ tanggal }}", $date, $html);
+
+
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+        $dompdf->add_info("Title", "An Example PDF");
+        $dompdf->stream('surat_pengajuan.pdf');
+    }
+
+    public function tgl_indo($tanggal){
+        $bulan = array (
+            1 =>   'Januari',
+            'Februari',
+            'Maret',
+            'April',
+            'Mei',
+            'Juni',
+            'Juli',
+            'Agustus',
+            'September',
+            'Oktober',
+            'November',
+            'Desember'
+        );
+        $pecahkan = explode('-', $tanggal);
+
+        return $pecahkan[2] . ' ' . $bulan[ (int)$pecahkan[1] ] . ' ' . $pecahkan[0];
     }
 }
